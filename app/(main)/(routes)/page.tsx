0fe15791a768678c6c330f6f562166b6fa0494cc
akins-dev <emmanuelakins908@gmail.com>
2024-11-03 20:24:21 +0100

    chore: Initail commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-indigo-500 text-3xl font-bold">
+      Hello discord clone
+    </div>
+  );
+}
