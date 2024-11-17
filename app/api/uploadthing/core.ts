8554f8bf28a0daa4319aaab9ed339fb6f50409bc
akins-dev <emmanuelakins908@gmail.com>
2024-11-17 15:49:45 +0100

    chore: finish uplouploadthing integration

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -14,12 +14,30 @@ const handleAuth = async () => {
 }
 
 export const ourFileRouter = {
-    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
+    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1, minFileCount: 1 } })
+        .middleware(() => handleAuth())
+        .onUploadComplete(async ({ metadata, file }) => {
+                // This code RUNS ON YOUR SERVER after upload
+                console.log("Upload complete for userId:", metadata.userId);
+            
+                console.log("file url", file.url);
+            
+                // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+                return { uploadedBy: metadata.userId, url: file.url };
+            }
+        ),
     messageFile: f(["image", "pdf"])
         .middleware(() => handleAuth())
-        .onUploadComplete(() => { })
+        .onUploadComplete(async ({ metadata, file }) => {
+                // This code RUNS ON YOUR SERVER after upload
+                console.log("Upload complete for userId:", metadata.userId);
+            
+                console.log("file url", file.url);
+            
+                // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+                return { uploadedBy: metadata.userId, url: file.url };
+            }
+        )
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
-import "@uploadthing/react/styles.css"
+// import "@uploadthing/react/styles.css"
 
 interface FileUploadProps {
   endpoint: "serverImage" | "messageFile";
@@ -23,17 +23,18 @@ const FileUpload = ({
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
-        <Image  
+        <Image
           src={value}
           alt="Upload"
+          fill
           className="rounded-full"
         />
-        <button 
+        <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
           type="button"
         >
-          <X  className="h-4 w-4" />
+          <X className="h-4 w-4" />
         </button>
       </div>
     )
